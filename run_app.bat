db81531c150cf51cee4e421852b8ff64357c0da6
Harshita0614 <mehtaharshita558@gmail.com>
2024-09-13 11:57:02 +0530

    icon addition

---
+++ run_app.bat
@@ -0,0 +1,3 @@
+@echo off
+cd C:\path\to\your\project  # Navigate to the folder where your Streamlit app is located
+streamlit run app.py  # Replace "app.py" with the name of your Streamlit app file
